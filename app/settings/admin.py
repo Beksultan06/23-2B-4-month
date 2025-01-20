326907023468e52e7d9aa7dcc5d7fde0d8eec6de
didiotghima <nurlanuuulubeksultan@gmail.com>
2025-01-20 19:58:39 +0600

    commit

--- app/settings/admin.py
+++ app/settings/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app.settings.models import Settings, Info
+from app.settings.models import Settings, Info, About
 # Register your models here.
 admin.site.register(Settings)
 admin.site.register(Info)
+admin.site.register(About)
--- app/settings/views.py
+++ app/settings/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from app.settings.models import Settings, Info
+from app.settings.models import Settings, Info, About
 
 def index(request):
     settings_id = Settings.objects.latest("id")
     info_all = Info.objects.all()
+    about = About.objects.latest("id")
     return render(request, "index.html", locals())
